3a401988967e9a2fd9ae3dd298430675076d1cb8
Khalel-SWE <khalel.swe96@gmail.com>
2025-04-05 15:52:34 -0700

    while changes

--- eWallet/eWalletSystem/src/service/Impl/ApplicationServiceImpl.java
+++ eWallet/eWalletSystem/src/service/Impl/ApplicationServiceImpl.java
@@ -10,26 +10,35 @@ public class ApplicationServiceImpl implements ApplicationService {
 
         System.out.println("------------> HI Sir I hope to be good <------------");
 
-        System.out.println("Please enter your choose...");
-        System.out.println("1.login      2.create account    3.exit");
+        int counter
+        while (true) {
+            System.out.println("Please enter your choose...");
+            System.out.println("1.login      2.create account    3.exit");
 
-        Scanner scanner = new Scanner(System.in);
-        int choose = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            int choose = scanner.nextInt();
 
-        switch (choose) {
-            case 1:
-                login();
+            boolean exit = false;
+            switch (choose) {
+                case 1:
+                    login();
+                    break;
+                case 2:
+                    createAccount();
+                    break;
+
+                case 3:
+                    System.out.println("Have a nice day Sir");
+                    exit = true;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice please try another one...");
+            }
+
+            if (exit) {
                 break;
-            case 2:
-                createAccount();
-                break;
-
-            case 3:
-                System.out.println("Have a nice day Sir");
-                break;
-
-            default:
-                System.out.println("Invalid choice please try another one...");
+            }
         }
     }
 
