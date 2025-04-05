a51f132878a80a5014fad90bb8c0b6d51f4f70f3
Khalel-SWE <khalel.swe96@gmail.com>
2025-04-05 15:07:56 -0700

    main class change

--- eWallet/eWalletSystem/src/Main.java
+++ eWallet/eWalletSystem/src/Main.java
@@ -1,31 +1,11 @@
-import java.util.Scanner;
+import service.ApplicationService;
+import service.Impl.ApplicationServiceImpl;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("------------> HI Sir I hope to be good <------------");
-
-        System.out.println("Please enter your choose...");
-        System.out.println("1.login      2.create account    3.exit");
-
-        Scanner scanner = new Scanner(System.in);
-        int choose = scanner.nextInt();
-
-        switch (choose) {
-            case 1:
-
-                break;
-            case 2:
-
-                break;
-
-            case 3:
-
-                break;
-
-            default:
-                System.out.println("Invalid choice please try another one...");
-        }
+        ApplicationService applicationService = new ApplicationServiceImpl();
+        applicationService.start();
     }
 }
--- eWallet/eWalletSystem/src/service/Impl/ApplicationServiceImpl.java
+++ eWallet/eWalletSystem/src/service/Impl/ApplicationServiceImpl.java
@@ -2,9 +2,42 @@ package service.Impl;
 
 import service.ApplicationService;
 
+import java.util.Scanner;
+
 public class ApplicationServiceImpl implements ApplicationService {
     @Override
     public void start() {
 
+        System.out.println("------------> HI Sir I hope to be good <------------");
+
+        System.out.println("Please enter your choose...");
+        System.out.println("1.login      2.create account    3.exit");
+
+        Scanner scanner = new Scanner(System.in);
+        int choose = scanner.nextInt();
+
+        switch (choose) {
+            case 1:
+                login();
+                break;
+            case 2:
+                createAccount();
+                break;
+
+            case 3:
+                System.out.println("Have a nice day Sir");
+                break;
+
+            default:
+                System.out.println("Invalid choice please try another one...");
+        }
+    }
+
+    private void login() {
+        System.out.println("login");
+    }
+
+    private void createAccount() {
+        System.out.println("create account");
     }
 }
